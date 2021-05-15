b3c8a9f579a3293d25f9b0448fe80ef722aa42f2
Hakan Altindag <hakangoudberg@hotmail.com>
2021-05-15 10:17:49 +0200

    Adjusted cli app name

--- Formula/crip.rb
+++ Formula/crip.rb
@@ -1,14 +1,14 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
-class Cerip < Formula
+class Crip < Formula
   desc "Extract server certificates. - CLI"
   homepage "https://github.com/Hakky54/certificate-ripper"
-  url "https://github.com/Hakky54/certificate-ripper/releases/download/0.0.1/cerip.tar.gz"
-  sha256 "fc6b3177b99d144252411f8bb95973d829146e2b40515b7aa9db2883a8bf800a"
+  url "https://github.com/Hakky54/certificate-ripper/releases/download/0.0.1/crip.tar.gz"
+  sha256 "ed571e6ddfa269ed202cb768806876bbe3ce279a0fba44ff255dd62325c375b8"
   license "Apache-2.0"
   version "0.0.1"
 
   def install
-    bin.install "cerip"
+    bin.install "crip"
   end
 end
