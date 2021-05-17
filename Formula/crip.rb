bb6c33e414ac8d8010c4b221d1273c692b46c3d8
Hakky54 <hakangoudberg@hotmail.com>
2021-05-17 10:17:07 +0200

    Update crip.rb

--- Formula/crip.rb
+++ Formula/crip.rb
@@ -3,7 +3,7 @@
 class Crip < Formula
   desc "Extract server certificates. - CLI"
   homepage "https://github.com/Hakky54/certificate-ripper"
-  url "https://github.com/Hakky54/certificate-ripper/releases/download/0.0.2/crip.tar.gz"
+  url "https://github.com/Hakky54/certificate-ripper/releases/download/0.0.2/crip-macos.tar.gz"
   sha256 "8372ddec84331680014cf59d5422771cb1a5ad3d544c247073efccfe8b8bddc5"
   license "Apache-2.0"
   version "0.0.2"
