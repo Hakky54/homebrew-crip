dc5b7d141cad1939dfe2777d048659b50d413e31
Hakan Altındağ <hakangoudberg@hotmail.com>
2021-08-31 17:34:40 +0200

    adjusted sha

--- Formula/crip.rb
+++ Formula/crip.rb
@@ -4,7 +4,7 @@ class Crip < Formula
   desc "Extract server certificates. - CLI"
   homepage "https://github.com/Hakky54/certificate-ripper"
   url "https://github.com/Hakky54/certificate-ripper/releases/download/1.0.0/crip-macos.tar.gz"
-  sha256 "c91acfb798b3462bf96d4757fd1eb3c0c967cfb42f3b7514743622eb2b23d661"
+  sha256 "afca33f6c572ebae1faa383f86923e5bd5c8b074a2c61b2da6b088d7bfa18944"
   license "Apache-2.0"
   version "1.0.0"
 
