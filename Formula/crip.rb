5fc7b34ef8b0437f51a3ec3f306712907994fcf8
Hakan Altındağ <hakangoudberg@hotmail.com>
2022-12-27 22:29:34 +0100

    corrected version

--- Formula/crip.rb
+++ Formula/crip.rb
@@ -6,7 +6,7 @@ class Crip < Formula
   url "https://github.com/Hakky54/certificate-ripper/releases/download/2.0.1/crip-macos-amd64.tar.gz"
   sha256 "f14909e7e0864db3949e92fb8af96caeba6c84536a2ec4c7241b4c827b56837f"
   license "Apache-2.0"
-  version "2.0.0"
+  version "2.0.1"
 
   def install
     bin.install "crip"
