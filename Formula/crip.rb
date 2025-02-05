c297fb1f63d2f66225d8728ca9133bb7d093e0f1
Hakan Altındağ <hakangoudberg@hotmail.com>
2025-02-05 21:46:46 +0100

    Bumped to 2.4.0

--- Formula/crip.rb
+++ Formula/crip.rb
@@ -3,10 +3,10 @@
 class Crip < Formula
   desc "Extract server certificates. - CLI"
   homepage "https://github.com/Hakky54/certificate-ripper"
-  url "https://github.com/Hakky54/certificate-ripper/releases/download/2.3.0/crip-macos-amd64.tar.gz"
-  sha256 "e0c8f1eb2724b7032ff6f70f4f7bbe38e79b260fab8d7d8ea925b87e299ee53a"
+  url "https://github.com/Hakky54/certificate-ripper/releases/download/2.4.0/crip-macos-amd64.tar.gz"
+  sha256 "d2bad0697da026118b67d4aae202b6eaf3cf07b527d552e3f6f1ea79cf66a7c3"
   license "Apache-2.0"
-  version "2.3.0"
+  version "2.4.0"
 
   def install
     bin.install "crip"
